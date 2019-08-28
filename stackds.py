5d83517391544ae7f0f0fd380ab293c74a8e01ba
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2019-08-28 15:38:08 -0400

    init___0.0.6

--- stackds.py
+++ stackds.py
@@ -27,6 +27,13 @@ except:
 	errormessage(1,"Missing Dependency: GTK")
 #__System__
 from IPython.display import clear_output, Image, display
+from shutil import copyfile
+from subprocess import Popen, PIPE
+try:
+	from cStringIO import StringIO
+except:
+	print('No String IO Functionality will be available')
+import imp
 #__Dataframes__
 try:
 	import pandas as pd
@@ -42,39 +49,43 @@ try:
 	import imgkit
 except:
 	errormessage(4,"Missing Dependency: imagekit")
-from shutil import copyfile
-from subprocess import Popen, PIPE
-from cStringIO import StringIO
-#TODO Future Dependencies:
-#dependency: Plotly Express
-#dependency: Sklearn
-#TODO install in install.sh
 #Dependency: sudo apt-get install wkhtmltopdf
+#______EXTENSIONS______
+print('Checking for zIpy...')
+try:
+	zIpy = imp.load_source('Myapp', 'extensions/test.py')
+	zIpymod = True
+except:
+	zIpymod = False
+	print('Module zIpy is not installed')
+print('Checking for Dash-Ly...')
+try:
+	dashlymod = imp.load_source('Myapp', 'extensions/dashly.py')
+	DashLymod = True
+except:
+	DashLymod = False
+	print('Checking for Dash-Ly...')
+	print('Module Dash-Ly is not installed')
+	if zIpymod == False:
+		print('Business as usual')
+		print('Running StackDS in Standalone Mode')
+#MACHINE LEARNING
+import SKLEARN
 UI_FILE = "src/stackds.ui"
 
 #<----Dependencies---->
 #=====TODO LIST=====
 #TODO UI Update? Buttons, better coloring,etc.
 #TODO add error GUI for application internal errors
-#TODO Preferences
 #TODO Add Hotkeys
-#TODO Function that places into GUI in treeview, or grid
-#TODO DataFrame Menu
-#TODO Add saved(ID) as boolean, to tell if DF is saved
-#TODO Machine Learning Menu???
-#TODO Data Studio Visualization(seperate class and UI file), plotly, w plotly
-#																		Dash
+#TODO DFParser, for another way to view the DataFrames without rendering images
+#TODO Add saved(ID) as boolean, to tell if DF is saved																	Dash
 #TODO Add confirm overwrite dialog
-#TODO Add Pipeline menu, with storable pipelines, maybe put this into ML menu
-#TODO Df parser, to place df into grid or treeview
-#TODO Add DF saving on tab switch, datablah.tocsv, id before update.
-#TODO Export HTML
+#TODO Selectable Df parser, to place df into grid or treeview
 #TODO Refresh ID, DF, ETC on close, as of right now, you have to switch and
 #												Reopen it...
 #TODO Change color of df_live_code Textbox
-#TODO New DF from series or Txt
-#TODO Preferences: Add Head Size
-#TODO Preferences: Add Debug mode to make more visible.
+#TODO UNNAMED- BUG
 #|		|		 || 		|	  |
 #=====TODO LIST=====
 #<======User Interface======>
@@ -92,9 +103,33 @@ class GUI:
 		self.id_counter = self.builder.get_object('id_counter')
 		self.dfid = 0
 		#--/Objects--
+		#==EXTENSIONS!==
+		if zIpymod == True:
+			pipelinezipy = self.builder.get_object('pipelinezipy')
+			self.exbar = self.builder.get_object('extbar')
+			zipy = self.builder.get_object('Zipy')
+			zipy.show()
+			self.exbar.show()
+			pipelinezipy.show()
+			self.fm_showext = self.builder.get_object('show_extbar')
+			self.fm_showext.show()
+			self.extbart = True
+			creinzip = self.builder.get_object('creinzip')
+			creinzip.show()
+		if DashLymod == True:
+			self.exbar = self.builder.get_object('extbar')
+			self.fm_showext = self.builder.get_object('show_extbar')
+			self.exbar.show()
+			dashly = self.builder.get_object('Dashly')
+			dashly.show()
+			self.extbart = True
+			self.fm_showext.show()
 		#--Environment-Variables--
+		#These Variables are defined on startup, to be
+		#Adjusted at the user's pleasure
 		self.notebookclosed = False
 		self.nbidmultiplier = 0
+		self.display_head = 10
 		#----Window----
 		#Gotta show the window, too
 		window.show_all()
@@ -160,18 +195,78 @@ class GUI:
 		df = self.dataframe
 		self.export_csv = self.builder.get_object('export_csv_dialog')
 		self.export_csv.show()
+	#<--------Export HTML-------->
+	def fm_exporthtml_cl(self,dialog):
+		htmlsave = self.builder.get_object('Save_HTML')
+		htmlsave.show()
 #<============DataFrame Menu======>
 	#<-------Manual Entry------->
 	def df_exec_mb(self,pasghetti):
 		self.df_live_code = self.builder.get_object('df_live_code')
 		self.df_exec_output = self.builder.get_object('df_exec_output')
-		#self.df_exec_output.modify_bg(Gtk.StateType.Normal,
-		#							Gdk.Color(20000, 10000, 10000))
+		self.df_exec_output.modify_base(Gtk.StateFlags.NORMAL,
+										Gdk.color_parse('green'))
+		self.df_exec_output.modify_text(Gtk.StateFlags.NORMAL,
+										Gdk.color_parse('white'))
 		#TODO Change Color^^
 		self.df_live_code.show()
 		self.dfexeci = self.builder.get_object('df_exec_input')
 		self.dfexeco = self.builder.get_object('df_exec_output')
-#
+	#<------Drop----->
+	def fm_df_drop_cl(self,spaghetti):
+		self.dropdialog = self.builder.get_object('drop_dialog')
+		self.packbox = self.builder.get_object('packbox')
+		self.drop_content_toggle = self.builder.get_object(
+													'drop_content_toggle')
+		self.drop_row_toggle = self.builder.get_object('drop_row_toggle')
+		self.drop_column_toggle = self.builder.get_object('drop_column_toggle')
+		self.dropdialog.show()
+	#<------Clean------>
+	def fm_clean_select(self,clean):
+		print('Clean')
+	#<-----Replace---->
+	def fm_df_replace(self,rep):
+		dfrep = self.builder.get_object('df_replace_dialog')
+		dfrep.show()
+		columnnumber = 1
+		df = self.dataframe
+		self.repcolrplc = self.builder.get_object('repcolrplc')
+		for (columnName, columnData) in df.iteritems():
+			columnparse = str(columnName)
+			columnnumber = str(columnnumber)
+			self.repcolrplc.append(columnnumber,columnparse)
+			columnnumber = int(columnnumber)
+			columnnumber = columnnumber+1
+#=================Pipelines=======>
+	def fm_pipelineshow(self,pip):
+		pipelines = self.builder.get_object('Pipelines')
+		pipelines.show()
+#=================Preferences=====>
+	#<------Show Preferences----->
+	def show_preferences(self,potat):
+		self.pref = self.builder.get_object('preferences')
+		verslabel = self.builder.get_object('prefverslabel')
+		versidfm = self.builder.get_object('VersionID')
+		portfolbut = self.builder.get_object('Portfolbutt')
+		docbut = self.builder.get_object('Documentationbut')
+		websbut = self.builder.get_object('Websitebut')
+		portfolbut.set_label('My Portfolio')
+		docbut.set_label('Documentation')
+		websbut.set_label('StackDS Website')
+		versid = versidfm.get_label()
+		verslabel.set_text(versid)
+		self.pref.show()
+	#<-----Show/Hide Extension bar----->
+	def fm_show_extbar(self,main):
+		if self.extbart == True:
+			self.exbar.hide()
+			self.fm_showext.set_label('Show Extensions')
+			self.extbart = False
+		else:
+			self.exbar.show()
+			self.fm_showext.set_label('Hide Extensions')
+			self.extbart = True
+
 #|	|	|	|	|	|	|	|	|	|	|	|	|	|
 #|	|	|	|	|	Dialogs	|	|	|	|	|	|	|
 #|	|	|	|	|	|	|	|	|	|	|	|	|	|
@@ -195,13 +290,13 @@ class GUI:
 		except:
 			print(errormessage(21,'File Imported is not a CSV file'))
 		print('File ',filename,' Read Successfully.')
-		self.display_head = 10
 		dfforconv = df.head(self.display_head)
 		convertimage(dfforconv,css,'swap.png')
 		dfimage = Gtk.Image.new_from_file('swap.png')
 		#Renders scrollbar with DF as image.
 		self.scroller = Gtk.ScrolledWindow.new()
 		self.scroller.add(dfimage)
+		self.scroller.set_focus_child(dfimage)
 		self.notebooklabel = Gtk.Label()
 		self.notebooklabel.set_text(self.filetop)
 		self.thenotebook.append_page(self.scroller,self.notebooklabel)
@@ -284,39 +379,134 @@ class GUI:
 	def df_exec_close(self,hel):
 		self.df_live_code.hide()
 	def df_exec_ex(self,spo):
+		outputbuffer = Gtk.TextBuffer.new()
 		try:
-			df = self.dataframe
+			df = self.dataframe.copy()
 		except:
-			outputbuffer = Gtk.TextBuffer.new()
-			outputbuffer.set_text('SqCode Fatal ERROR!: Data not loaded')
-			self.dfexeco.set_buffer(outputbuffer)
-		sys.stdout = buffer = StringIO()
-		print('***********************')
-		print('===zIpy Lite 0.0.1===')
-		print('***********************')
+			outputbuffer.set_text('zIpy Fatal ERROR!: Data not loaded')
+		self.dfexeco.set_buffer(outputbuffer)
 		userexec = self.dfexeci.get_text()
+		sys.stdout = buffer = StringIO()
+		print('*********************')
+		print('===zIpy Lite 0.0.1===')
+		print('*********************')
 		try:
 			exec(userexec)
 		except:
-			print('Code contains error!')
-		outputbuffer = Gtk.TextBuffer.new()
+			print('zIpy Lite has found an error!')
+			outputbuffer.set_text(buffer.getvalue())
 		outputbuffer.set_text(buffer.getvalue())
 		self.dfexeco.set_buffer(outputbuffer)
 		self.updatedataview(df)
+	#<-------Drop-------->
+	def df_drop_click(self,ppe):
+		df = self.dataframe
+		ide = self.packbox.get_active_text()
+		if self.columndrop == True:
+			df = df.drop(columns=[ide])
+		if self.rowdrop == True:
+			df = df.drop(df.index[int(ide)])
+		if self.contentdrop == True:
+			df = df.drop(df.str.contains(ide))
+		self.updatedataview(df)
+		self.dropdialog.show()
+	def Df_Drop_Cancel(self,ppc):
+		self.dropdialog.hide()
+	def on_drop_row_t(self,wwc):
+		columnnumber = 1
+		df = self.dataframe
+		self.rowdrop = True
+		self.contentdrop = False
+		self.columndrop = False
+		self.drop_content_toggle.set_active(False)
+		self.drop_column_toggle.set_active(False)
+		self.packbox.remove_all()
+		for i in df.index:
+			columntitle = str(i)
+			columnnumber = str(columnnumber)
+			self.packbox.append(columnnumber,columntitle)
+			columnnumber = int(columnnumber)
+			columnnumber = columnnumber+1
+	def on_drop_content_t(self,wwe):
+		self.rowdrop = False
+		self.contentdrop = True
+		self.columndrop = False
+		self.drop_row_toggle.set_active(False)
+		self.drop_column_toggle.set_active(False)
+		self.Content_drop = self.builder.get_object('Content_Drop_Dialog')
+		self.Content_drop.show()
+		self.packbox.remove_all()
+		self.packbox.Has_Entry = True
+	def on_drop_column_t(self,wwb):
+		self.columndrop = True
+		self.contentdrop = False
+		self.rowdrop = False
+		self.drop_content_toggle.set_active(False)
+		self.drop_row_toggle.set_active(False)
+		self.packbox.remove_all()
+		columnnumber = 1
+		df = self.dataframe
+		for (columnName, columnData) in df.iteritems():
+			columnparse = str(columnName)
+			columnnumber = str(columnnumber)
+   			self.packbox.append(columnnumber,columnparse)
+			columnnumber = int(columnnumber)
+			columnnumber = columnnumber+1
+	def content_drop_b(self,eat):
+		self.contenttodrop = self.builder.get_object('contenttodrop')
+		self.packbox.append(str(1),self.contenttodrop.get_text())
+		self.Content_drop.hide()
+	#<------DF Replace------->
+	#TODO Add error dialog for entries not found on axis
+	def df_column_replace(self,ricky):
+		df = self.dataframe
+		toreplace = self.builder.get_object('replcolent')
+		hl = toreplace.get_text()
+		replme = self.repcolrplc.get_active_text()
+		df = df.rename({replme : hl})
+		self.updatedataview(df)
+	def df_cont_repl(self,jam):
+		toreplacecont = self.builder.get_object('toreplacecont')
+		withcont = self.builder.get_object('withcont')
+		df = df.replace(toreplacecont,withcont)
+	def df_repl_cancel(self,pot):
+		dfrep = self.builder.get_object('df_replace_dialog')
+		dfrep.hide()
+	#Export To HTML
+		
+	#<------Preferences------>
+	def preference_cl(self,cl):
+		self.pref.hide()
+	#<------Export HTML------>
+	def save_html_conf(self, der):		
+		save_html = self.builder.get_object('Save_HTML')
+		htmllabel = self.builder.get_object('Save_HTML_Label')
+		label = htmllabel.get_text()
+		filepath = save_html.get_current_folder
+		savefile = str(filepath)+str(label)+'.html'
+		dfhtml = self.dataframe.to_html()
+		html = css+str(dfhtml)
+		open(savefile, 'w').close()
+		text_file = open(savefile, "a")
+		text_file.write(html)
+		text_file.close()
+		winder = self.builder.get_object('Save_HTML')
+		winder.hide()
+	def save_html_Cancel(self, peer):
+		winder = self.builder.get_object('Save_HTML')
+		winder.hide()
 	#_____________________________________
 	#<<<<<<<<<Class Accessories>>>>>>>>>>>
 	def updatedataview(self,df):
 		df.to_csv(self.swapfileactive)
-		index = self.indexid
+		self.dataframe = pd.read_csv(self.swapfileactive)
 		dfforconv = df.head(self.display_head)
+		scroller = Gtk.ScrolledWindow.new()
 		convertimage(dfforconv,css,'swap.png')
-		self.dfimage = Gtk.Image.new_from_file('swap.png')
-		self.scroller = Gtk.ScrolledWindow.new()
-		self.scroller.add(dfimage)
-		self.labeler = Gtk.Label.new()
-		self.labeler.set_text('df'+str(index+1))
-		self.thenotebook.append_page(scroller, labeler)
-		self.fm_closetab_select(self.indexid)
+		page_num = self.id_counter.get_label()
+		scrolleroller = self.thenotebook.get_nth_page(int(page_num))
+		dfrealimage = scrolleroller.get_focus_child()
+		dfrealimage.set_from_file('swap.png')
 #========================================
 #<---------Accessory Functions---------->
 #TODO DFPARSER FOR NON IMAGE DF
